6b0848f81406862873c76ada518c57a48c49b361
Gil Sala <gilsala_88@hotmail.com>
2023-04-17 09:17:49 +0200

    Add increase and decrease buttons functionality

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,30 +1,104 @@
 import Panel from "../Panel/Panel";
+import { useState, useRef } from "react";
+import "./Form.css";
+import { useEffect } from "react";
 
 const Form = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const [websitePrice, setWebsitePrice] = useState({webSitePrice:false, value: 0});
+  const [seoPrice, setSeoPrice] = useState({seoPrice:false, value: 0});
+  const [adsPrice, setAdsPrice] = useState({adsPrice:false, value: 0});
+  
+
+ 
+
+  const handleCheckBoxWebsite = (e) => {
+    setIsClicked(!isClicked)
+    updateCheckbox(e, websitePrice, setWebsitePrice);
+    console.log(websitePrice);
+  };
+
+  const handleCheckBoxSeo = (e) => {
+    updateCheckbox(e, seoPrice, setSeoPrice);
+  };
+
+  const handleCheckBoxAds = (e) => {
+    updateCheckbox(e, adsPrice, setAdsPrice);
+  };
+
+  const setNumberOfPages=(e)=>(e.target.value)
+  const setNumberOfLanguages=(e)=>(e.target.value)
+  
+  const priceDom = websitePrice.value + seoPrice.value + adsPrice.value
+  useEffect(() => {
+    console.log(websitePrice, seoPrice, adsPrice);
+  }, [websitePrice, seoPrice, adsPrice, priceDom]);
+
+  
+  const updateCheckbox = (e, checkbox, setCheckBox) => {
+    if (e.target.checked === false) {
+      setCheckBox({[e.target.name]: false, value: 0 });
+    } else {
+      setCheckBox({[e.target.name]: true, value: Number(e.target.value)  });
+    }
+  };
+
+  
+
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(userRef.current.value, budgetRef.current.value);
+  };
+  const userRef = useRef();
+  const budgetRef = useRef();
+
   return (
     <>
-      <h1>Enter you data</h1>
-      <form className="form" action="">
+      <form className="form" action="" onSubmit={onSubmit}>
         <h3>Personal information</h3>
         <div className="personal-information">
-          <div className="name" style={{display:"block"}}>
-          <label htmlFor="user-name">User's name</label>
-          <input type="text" />
+          <div className="name" style={{ display: "block" }}>
+            <label htmlFor="user-name">User's name</label>
+            <input type="text" ref={userRef} />
           </div>
           <label htmlFor="budget-name">Budget's name</label>
-          <input type="text" />
+          <input type="text" ref={budgetRef} />
         </div>
         <div className="services">
           <h3> What service would you like to hire?</h3>
+          <input
+            checked={websitePrice.webSitePrice}
+            onChange={handleCheckBoxWebsite}
+            type="checkbox"
+            id="website"
+            name="website"
+            value="500"
+          />
           <label htmlFor="website">A website (500 €)</label>
-          <input type="checkbox" id="website" name="website" value="500" />
-          <Panel />
-          <input type="checkbox" id="seo" name="seo" value="300" />
+          <div className={isClicked ? "modal-displayed" : "modal-hidden"}>
+            <Panel setNumberOfLanguages={setNumberOfLanguages} setNumberOfPages={setNumberOfPages}/>
+          </div>
+          <input
+            checked={seoPrice.seoPrice}
+            onChange={handleCheckBoxSeo}
+            type="checkbox"
+            id="seo"
+            name="seo"
+            value="300"
+          />
           <label htmlFor="seo">Seo consultant (300 €)</label>
-          <input type="checkbox" id="adds" name="adds" value="200" />
+          <input
+            checked={adsPrice.adsPrice}
+            onChange={handleCheckBoxAds}
+            type="checkbox"
+            id="adds"
+            name="adds"
+            value="200"
+          />
           <label htmlFor="seo">Google ads campaign (200 €)</label>
         </div>
-        <p name="price">Price : 0 € </p>
+        <p name="price">Price : {priceDom} € </p>
         <button>Show budget</button>
       </form>
     </>
@@ -32,3 +106,15 @@ const Form = () => {
 };
 
 export default Form;
+
+
+
+/*const updateCheckbox = (e, checkbox, setCheckBox) => {
+    setCheckBox({
+      
+      [e.target.name]: !checkbox[e.target.name], // toggle the state of the checkbox
+      value: checkbox[e.target.name] ? Number(e.target.value) : 0  // update the checkbox value based on its state
+      
+    });
+    console.log(checkbox[e.target.name])
+  }*/
--- src/Components/Panel/Panel.js
+++ src/Components/Panel/Panel.js
@@ -1,25 +1,60 @@
 import { StyledPanel, Pages } from "./styledPanel";
 import InfoLanguages from "../Info/InfoLanguages";
 import InfoPages from "../Info/InfoPages";
+import { useState } from "react";
 
 const Panel = () => {
+
+const [numberOfPages, setNumberOfPages] = useState("")
+const [numberOfLanguages, setNumberOfLanguages] =useState("")
+
+const decreasePage= (amount)=>{
+  setNumberOfPages(currentCount =>{
+    console.log(currentCount + amount, typeof amount, typeof currentCount)
+    return Number(currentCount) - amount
+  })
+}
+
+   
+const increasePage =(amount)=>{
+  setNumberOfPages(currentCount =>{
+    console.log(currentCount + amount, typeof amount, typeof currentCount)
+    return Number(currentCount) + amount
+  })
+}
+
+
+const decreaseLanguage= (amount)=>{
+  setNumberOfLanguages(currentCount =>{
+    console.log(currentCount - amount)
+    return Number(currentCount) - amount
+  })
+}
+
+const increaseLanguage =(amount)=>{
+  setNumberOfLanguages(currentCount =>{
+    console.log(currentCount + amount)
+    return Number(currentCount) + amount
+  })
+}
+
+
   return (
     <>
-      <StyledPanel>
+      <StyledPanel >
         <Pages>
           <label htmlFor="pages">Number of pages</label>
-          <button type="button">-</button>
-          <input id="pages" value={""} type="number" />
-
-          <button type="button">+</button>
+          <button type="button" onClick={()=>decreasePage(1)}>-</button>
+          <input id="pages"min={0} onChange={(e)=>setNumberOfPages(e.target.value)} value ={numberOfPages} type="number" />
+          <button type="button" onClick={()=>increasePage(1)}>+</button>
           <InfoPages />
         </Pages>
 
         <Pages>
           <label htmlFor="languages">Number of languages</label>
-          <button type="button">-</button>
-          <input value={""} type="number" />
-          <button type="button">+</button>
+          <button type="button" onClick={()=>decreaseLanguage(1)}>-</button>
+          <input value={numberOfLanguages} onChange={(e)=>setNumberOfLanguages(e.target.value)} type="number" />
+          <button type="button" onClick={()=>increaseLanguage(1)} >+</button>
           <InfoLanguages />
         </Pages>
       </StyledPanel>
