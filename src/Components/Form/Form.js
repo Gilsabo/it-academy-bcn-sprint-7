a4a27176a175a4539a6de2e17e876d401002450c
Gil Sala <gilsala_88@hotmail.com>
2023-04-21 20:17:26 +0200

    Add useLocalStorage to handel form

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,110 +1,35 @@
 import Panel from "../Panel/Panel";
-import { useState, useRef } from "react";
+import { useRef } from "react";
 import "./Form.css";
 import { formContext } from "../helper";
-import { useEffect } from "react";
+import useCheckboxes from "../useLocalStorage/checkboxes";
 
 const Form = () => {
-  const [isClicked, setIsClicked] = useState(false);
-  const [totalPrice, setTotalPrice] = useState();
-  const [numberOfLanguages, setNumberOfLanguages] = useState(0);
-  const [numberOfPages, setNumberOfPages] = useState(0);
-
-  const [websitePrice, setWebsitePrice] = useState({value: 0 });
-  const [seoPrice, setSeoPrice] = useState({ value: 0 });
-  const [adsPrice, setAdsPrice] = useState({  value: 0 });
-   const [arrayState, setArrayState] = useState([]);
-  const [isChecked, setIsChecked] = useState({websitePrice :  false, seoPrice: false, adsPrice: false  })
-
-  const parsedArrayState = JSON.parse(localStorage.getItem("data"));
-
-  useEffect(() => {
-    setArrayState([
-      isChecked,
-      isClicked,
-      websitePrice,
-      seoPrice,
-      adsPrice,
-      numberOfPages,
-      numberOfLanguages,
-      totalPrice,
-    ]);
-    
-  }, [
+  const {
+    handleCheckBoxAds,
+    handleCheckBoxSeo,
+    handleCheckBoxWebsite,
+    arrayState,
     isChecked,
     isClicked,
-    websitePrice,
-    adsPrice,
-    seoPrice,
-    totalPrice,
-    numberOfLanguages,
     numberOfPages,
-  ]);
-
-  useEffect(()=>{
-    const data = arrayState
-    localStorage.setItem("data", JSON.stringify(data));
-    console.log(data)
-  },[arrayState])
- 
-
-  useEffect(() => {
-    
-    if (parsedArrayState) {
-      setArrayState(parsedArrayState);
-      setIsChecked(parsedArrayState[0])
-      setIsClicked(parsedArrayState[1]);
-      setWebsitePrice(parsedArrayState[2]);
-      setSeoPrice(parsedArrayState[3]);
-      setAdsPrice(parsedArrayState[4]);
-      setNumberOfPages(parsedArrayState[5]);
-      setNumberOfLanguages(parsedArrayState[6]);
-      setTotalPrice(parsedArrayState[7]);
-      console.log(1);
-    }
-    
-    
-  }, []);
-
-  const handleCheckBoxWebsite = (e) => {
-    setIsClicked(!isClicked)
-    updateCheckbox(e, setWebsitePrice);
-  };
-
-  const handleCheckBoxSeo = (e) => {
-    updateCheckbox(e, setSeoPrice);
-  };
-
-  const handleCheckBoxAds = (e) => {
-    updateCheckbox(e, setAdsPrice);
-  };
-
-  useEffect(() => {
-    setTotalPrice(
-      websitePrice.value +
-        seoPrice.value +
-        adsPrice.value +
-        numberOfPages * 30 +
-        numberOfLanguages * 30
-    );
-  }, [websitePrice, seoPrice, adsPrice, numberOfLanguages, numberOfPages]);
-
-  const updateCheckbox = (e, setCheckBox) => {
-    if (e.target.checked === true) {
-      setCheckBox({  value: Number(e.target.value) });
-      setIsChecked({...isChecked,[e.target.name]:true})
-      
-    } else {
-      setCheckBox({  value: 0 });
-      setIsChecked({...isChecked, [e.target.name]:false})
-    }
-  };
+    numberOfLanguages,
+    totalPrice,
+    setNumberOfLanguages,
+    setNumberOfPages,
+  } = useCheckboxes();
 
   const onSubmit = (e) => {
     e.preventDefault();
     console.log(userRef.current.value, budgetRef.current.value);
-    const budget = [{user:userRef.current.value, budgetname:budgetRef.current.value, info:arrayState }]
-    console.log(budget)
+    const budget = [
+      {
+        user: userRef.current.value,
+        budgetname: budgetRef.current.value,
+        info: arrayState,
+      },
+    ];
+    console.log(budget);
   };
   const userRef = useRef();
   const budgetRef = useRef();
@@ -132,7 +57,6 @@ const Form = () => {
           <h3> What service would you like to hire?</h3>
           <input
             checked={isChecked.websitePrice}
-            
             onClick={handleCheckBoxWebsite}
             type="checkbox"
             id="websitePrice"
@@ -145,7 +69,6 @@ const Form = () => {
           </div>
           <input
             checked={isChecked.seoPrice}
-            
             onClick={handleCheckBoxSeo}
             type="checkbox"
             id="seoPrice"
@@ -155,7 +78,6 @@ const Form = () => {
           <label htmlFor="seoPrice">Seo consultant (300 €)</label>
           <input
             checked={isChecked.adsPrice}
-            
             onClick={handleCheckBoxAds}
             type="checkbox"
             id="adsPrice"
@@ -172,5 +94,3 @@ const Form = () => {
 };
 
 export default Form;
-
-
---
+++ src/Components/useLocalStorage/checkboxes.js
@@ -0,0 +1,113 @@
+import { useState, useEffect } from "react";
+
+const useCheckboxes = () => {
+  const [websitePrice, setWebsitePrice] = useState({ value: 0 });
+  const [seoPrice, setSeoPrice] = useState({ value: 0 });
+  const [adsPrice, setAdsPrice] = useState({ value: 0 });
+  const [isClicked, setIsClicked] = useState(false);
+  const [totalPrice, setTotalPrice] = useState();
+  const [numberOfLanguages, setNumberOfLanguages] = useState(0);
+  const [numberOfPages, setNumberOfPages] = useState(0);
+
+  const [arrayState, setArrayState] = useState([]);
+  const [isChecked, setIsChecked] = useState({
+    websitePrice: false,
+    seoPrice: false,
+    adsPrice: false,
+  });
+
+  const parsedArrayState = JSON.parse(localStorage.getItem("data"));
+
+  useEffect(() => {
+    setArrayState([
+      isChecked,
+      isClicked,
+      websitePrice,
+      seoPrice,
+      adsPrice,
+      numberOfPages,
+      numberOfLanguages,
+      totalPrice,
+    ]);
+  }, [
+    isChecked,
+    isClicked,
+    websitePrice,
+    adsPrice,
+    seoPrice,
+    totalPrice,
+    numberOfLanguages,
+    numberOfPages,
+  ]);
+
+  useEffect(() => {
+    const data = arrayState;
+    localStorage.setItem("data", JSON.stringify(data));
+    console.log(data);
+  }, [arrayState]);
+
+  useEffect(() => {
+    if (parsedArrayState) {
+      setArrayState(parsedArrayState);
+      setIsChecked(parsedArrayState[0]);
+      setIsClicked(parsedArrayState[1]);
+      setWebsitePrice(parsedArrayState[2]);
+      setSeoPrice(parsedArrayState[3]);
+      setAdsPrice(parsedArrayState[4]);
+      setNumberOfPages(parsedArrayState[5]);
+      setNumberOfLanguages(parsedArrayState[6]);
+      setTotalPrice(parsedArrayState[7]);
+      console.log(1);
+    }
+  }, []);
+
+  const handleCheckBoxWebsite = (e) => {
+    setIsClicked(!isClicked);
+    updateCheckbox(e, setWebsitePrice);
+    console.log(websitePrice);
+  };
+
+  const handleCheckBoxSeo = (e) => {
+    updateCheckbox(e, setSeoPrice);
+  };
+
+  const handleCheckBoxAds = (e) => {
+    updateCheckbox(e, setAdsPrice);
+  };
+
+  useEffect(() => {
+    setTotalPrice(
+      websitePrice.value +
+        seoPrice.value +
+        adsPrice.value +
+        numberOfPages * 30 +
+        numberOfLanguages * 30
+    );
+  }, [websitePrice, seoPrice, adsPrice, numberOfLanguages, numberOfPages]);
+
+  const updateCheckbox = (e, setCheckBox) => {
+    if (e.target.checked === true) {
+      setCheckBox({ value: Number(e.target.value) });
+      setIsChecked({ ...isChecked, [e.target.name]: true });
+    } else {
+      setCheckBox({ value: 0 });
+      setIsChecked({ ...isChecked, [e.target.name]: false });
+    }
+  };
+
+  return {
+    handleCheckBoxAds,
+    handleCheckBoxSeo,
+    handleCheckBoxWebsite,
+    arrayState,
+    isChecked,
+    isClicked,
+    numberOfPages,
+    numberOfLanguages,
+    totalPrice,
+    setNumberOfLanguages,
+    setNumberOfPages,
+  };
+};
+
+export default useCheckboxes;
